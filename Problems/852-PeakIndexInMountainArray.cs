542769b295ee2fe5d13009be77e81524c083a9df
Ramesh Choudhary <itsc.ramesh@gmail.com>
2023-10-02 19:03:55 +0530

    Problem: 852 - Peak Index in mountain array

---
+++ Problems/852-PeakIndexInMountainArray.cs
@@ -0,0 +1,22 @@
+public class Solution
+{
+    public int PeakIndexInMountainArray(int[] arr)
+    {
+        int left = 0; int right = arr.Length - 1;
+
+        while (left < right)
+        {
+            int mid = left + (right - left) / 2;
+
+            if (arr[mid] > arr[mid + 1])
+            {
+                right = mid;
+            }
+            else
+            {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+}
